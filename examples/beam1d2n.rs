ee73acb8cba791d7eae05934728c52e287f110ae
zhm <1101510340zhm@gmail.com>
2023-08-01 11:48:37 +0800

    start Beam1D2N elem day3

--- examples/beam1d2n.rs
+++ examples/beam1d2n.rs
@@ -8,9 +8,35 @@ fn main() {
     let time_start = Instant::now();
 
     // set material parameters
-    let moment_of_inertia = 1.0 as Dtype;
-    let material = (1.0 as Dtype, 0.25 as Dtype);
+    let moi = 0.0001186 as Dtype;
+    let material = ((2.0 * 100000000000.0) as Dtype, 0.25 as Dtype);
 
-    let coords: Vec<Vec<Dtype>> = vec![vec![0.0, 0.0], [1.0, 0.0]];
+    /*
+    let coords: Vec<Vec<Dtype>> = vec![vec![0.0, 0.0], vec![1.0, 0.0]];
     let cpld: Vec<Vec<usize>> = vec![vec![1, 2]];
+    let zero_disp: Vec<usize> = vec![0, 1];
+    let force_index: Vec<usize> = vec![2];
+    let force_value: Vec<Dtype> = vec![10.0];
+    let force_data: HashMap<usize, Dtype> = force_index
+        .into_iter()
+        .zip(force_value.into_iter())
+        .collect();
+    */
+
+    let node1 = Node2D::new(0, [0.0, 0.0]);
+    let node2 = Node2D::new(1, [5.0, 0.0]);
+    let node3 = Node2D::new(1, [7.5, 0.0]);
+
+    let beam1 = Beam1D2N::new(1, moi, 0.00665, [&node1, &node2]);
+    let beam2 = Beam1D2N::new(2, moi, 0.00665, [&node2, &node3]);
+    print!("{}", beam1);
+    print!("{}", beam1);
+
+    let k1 = beam1.calc_k(material);
+    let k2 = beam2.calc_k(material);
+    print_2darr("beam k mat", &k1);
+    print_2darr("beam k mat", &k2);
+
+    let total_time = time_start.elapsed();
+    println!("\n>>> Total time consuming: {:?}", total_time);
 }
--- src/elem/dim1/beam.rs
+++ src/elem/dim1/beam.rs
@@ -5,10 +5,11 @@ use std::fmt::{self, Write};
 /// 细长梁以及纯弯梁，抽象成具有如下特征的理论模型：
 ///   1) 仅用x坐标刻画所有位置(即细长之含义)；
 ///   2) 变形主要为垂直于x轴的挠度
-/// 但是，节点的自由度不为1，而包含：挠度(y)、转角theta，
+/// 但是，节点的自由度不为1，包含：挠度(y)、转角(theta)，
 /// 此亦是梁单元为非协调单元的原因。
 pub struct Beam1D2N<'beam1d2n> {
     pub id: usize,
+    pub moi: Dtype, // moment of inertia
     pub sec_area: Dtype,
     pub nodes: [&'beam1d2n Node2D; 2],
     pub k_matrix: Option<[[Dtype; 4]; 4]>,
@@ -16,9 +17,10 @@ pub struct Beam1D2N<'beam1d2n> {
 
 impl<'beam1d2n> Beam1D2N<'beam1d2n> {
     /// Generate a Beam1D2N element
-    pub fn new(id: usize, sec_area: Dtype, nodes: [&Node2D; 2]) -> Beam1D2N {
+    pub fn new(id: usize, moi: Dtype, sec_area: Dtype, nodes: [&Node2D; 2]) -> Beam1D2N {
         Beam1D2N {
             id,
+            moi,
             sec_area,
             nodes,
             k_matrix: None,
@@ -66,6 +68,21 @@ impl<'beam1d2n> Beam1D2N<'beam1d2n> {
         forces
     }
 
+    /// Get point(in beam element)'s disp
+    /// 用节点的位移插值得到单元内任意点的位移，由于是纯弯梁
+    /// 这里的位移指每个节点的纵向位移v和转角\theta
+    pub fn point_disp(&self, point_coord: [Dtype; 1]) -> [Dtype; 2] {
+        let x = point_coord[0];
+        let n0 = self.shape_mat_i(0usize)(x);
+        let n1 = self.shape_mat_i(1usize)(x);
+        let n2 = self.shape_mat_i(2usize)(x);
+        let n3 = self.shape_mat_i(3usize)(x);
+        let disps = self.disps();
+        let v = n0 * disps[0] + n2 * disps[2];
+        let theta = n1 * disps[1] + n3 * disps[3];
+        [v, theta]
+    }
+
     /// Shape matrix element N_i
     /// The shape mat of Beam1D2N element: [N1 N2 N3 N4]
     /// which is a 1x4 mat.    \xi = x/L
@@ -74,7 +91,7 @@ impl<'beam1d2n> Beam1D2N<'beam1d2n> {
     /// N3 = 1*(0 + 0*\xi + 3*\xi^2 - 2*\xi^3)
     /// N4 = L*(0 + 0*\xi - 1*\xi^2 + 1*\xi^3)
     fn shape_mat_i(&self, i: usize) -> impl Fn(Dtype) -> Dtype {
-        // 参数i的取值范围：1～4
+        // 参数i的取值范围：0～3
         let l = self.length();
         let l2 = l * l;
         let l3 = l * l * l;
@@ -86,40 +103,164 @@ impl<'beam1d2n> Beam1D2N<'beam1d2n> {
     }
 
     /// Geometry matrix element B_i
-    fn geometry_mat(&self, x: Dtype) -> SMatrix<Dtype, 1, 4> {
+    fn geometry_mat(&self, xi: Dtype) -> SMatrix<Dtype, 1, 4> {
         let l = self.length();
-        let l2 = l * l;
-        let xi = x / l;
         SMatrix::<Dtype, 1, 4>::from([
-            [(12.0 * xi - 6.0) / l2],
-            [(6.0 * xi - 4.0) / l],
-            [-(12.0 * xi - 6.0) / l2],
-            [(6.0 * xi - 2.0) / l],
+            [12.0 * xi - 6.0],
+            [(6.0 * xi - 4.0) * l],
+            [-12.0 * xi + 6.0],
+            [(6.0 * xi - 2.0) * l],
         ])
     }
 
     /// Calculate element stiffness matrix K
     /// arg moi: moment of inertia
     /// return a 4x4 matrix, elements are Dtype
-    fn calc_k(&self, material_args: (Dtype, Dtype), moi: Dtype) -> [[Dtype; 4]; 4] {
+    pub fn calc_k(&self, material_args: (Dtype, Dtype)) -> [[Dtype; 4]; 4] {
         println!(
             "\n>>> Calculating Beam1D2N(#{})'s stiffness matrix k{} ......",
             self.id, self.id
         );
 
-        let (ee, nu) = material_args;
-        let l3 = self.length() * self.length() * self.length();
-        let gauss_pt = (3.0 as Dtype).sqrt() / (6.0 as Dtype);
+        let moi = self.moi;
+        let (ee, _nu) = material_args;
+        let l = self.length();
+        let l3 = l * l * l;
+        let gauss_pt: Dtype = (3.0).sqrt() / 6.0;
         let int_pts: [Dtype; 2] = [0.5 - gauss_pt, 0.5 + gauss_pt];
 
         let mut k_matrix = SMatrix::<Dtype, 4, 4>::from([[0.0; 4]; 4]);
         for idx in 0..2 {
             let b_mat = self.geometry_mat(int_pts[idx]);
-            let core = b_mat.transpose() * b_mat;
-            k_matrix += core;
+            let core = 0.5 * b_mat.transpose() * b_mat;
+            k_matrix = k_matrix + core;
         }
 
         let stiffness_matrix: [[Dtype; 4]; 4] = (ee * moi * k_matrix / l3).into();
         stiffness_matrix
+    }
+
+    /// Get the strain at point (x) inside the element
+    pub fn calc_strain(&self, xy: [Dtype; 2]) -> [Dtype; 1] {
+        let y = xy[1];
+        let xi = xy[0] / self.length();
+        let b_mat = self.geometry_mat(xi);
+        let elem_node_disps = SMatrix::<Dtype, 4, 1>::from(self.disps());
+        let strain_vector: [Dtype; 1] = (-y * b_mat * elem_node_disps).into();
+        strain_vector
+    }
+
+    /// Get the stress at point (x) inside the element
+    pub fn calc_stress(&self, xy: [Dtype; 2], material_args: (Dtype, Dtype)) -> [Dtype; 1] {
+        let (ee, _nu) = material_args;
+        [ee * self.calc_strain(xy)[0]]
+    }
+}
+
+/// Implement zhm::K trait for beam1d2n element
+impl<'beam1d2n> K for Beam1D2N<'beam1d2n> {
+    type Kmatrix = [[Dtype; 4]; 4];
+
+    /// Cache stiffness matrix for beam1d2n element
+    fn k(&mut self, material: (Dtype, Dtype)) -> &Self::Kmatrix
+    where
+        Self::Kmatrix: std::ops::Index<usize>,
+    {
+        if self.k_matrix.is_none() {
+            self.k_matrix.get_or_insert(self.calc_k(material))
+        } else {
+            self.k_matrix.as_ref().unwrap()
+        }
+    }
+
+    /// Print beam1d2n element's stiffness matrix
+    fn k_printer(&self, n_exp: Dtype) {
+        if self.k_matrix.is_none() {
+            panic!(
+                "!!! Beam1D2N#{}'s k mat is empty! call k() to calc it.",
+                self.id
+            );
+        }
+
+        print!("\n Beam1D2N k{} = (* 10^{})\n[", self.id, n_exp as u8);
+        for row in 0..4 {
+            if row == 0 {
+                print!("[");
+            } else {
+                print!(" [");
+            }
+            for col in 0..4 {
+                print!(
+                    " {:>-10.6} ",
+                    self.k_matrix.unwrap()[row][col] / (10.0_f64.powf(n_exp as f64)) as Dtype
+                );
+            }
+            if row == 3 {
+                print!("]]");
+            } else {
+                print!("]");
+            }
+        }
+        print!("\n");
+    }
+
+    /// Return Beam1D2N elem's stiffness matrix's format string
+    fn k_string(&self, n_exp: Dtype) -> String {
+        let mut k_matrix = String::new();
+        for row in 0..4 {
+            if row == 0 {
+                write!(k_matrix, "[[").expect("!!! Write tri k_mat failed!");
+            } else {
+                write!(k_matrix, " [").expect("!!! Write tri k_mat failed!");
+            }
+            for col in 0..4 {
+                write!(
+                    k_matrix,
+                    " {:>-10.6} ",
+                    self.k_matrix.unwrap()[row][col] / (10.0_f64.powf(n_exp as f64)) as Dtype
+                )
+                .expect("!!! Write element's stiffness mat string failed.");
+            }
+            if row == 3 {
+                write!(k_matrix, "]]").expect("!!! Write tri k_mat failed!");
+            } else {
+                write!(k_matrix, "]\n").expect("!!! Write tri k_mat failed!");
+            }
+        }
+        k_matrix
+    }
+
+    /// Get the strain at point (x) inside the element
+    fn strain(&self, xy: [Dtype; 3]) -> Vec<Dtype> {
+        self.calc_strain([xy[0], xy[1]]).to_vec()
+    }
+
+    /// Get the stress at point (x) inside the element
+    fn stress(&self, xy: [Dtype; 3], material_args: (Dtype, Dtype)) -> Vec<Dtype> {
+        self.calc_stress([xy[0], xy[1]], material_args).to_vec()
+    }
+
+    /// Get element's info string
+    fn info(&self) -> String {
+        format!(
+            "\n--------------------------------------------------------------------\nElement_1D Info:\n\tId:     {}\n\tCrossArea:   {}\n\tType:   Beam1D2N
+\tNodes: {}\n\t       {}\n\t",self.id, self.sec_area, self.nodes[0], self.nodes[1],
+        )
+    }
+
+    /// Get element's id number
+    fn id(&self) -> usize {
+        self.id
+    }
+}
+
+impl fmt::Display for Beam1D2N<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "\nElement_1D Info:\n\tId:     {}\n\tArea:   {}\n\tType:   Beam1D2N (Euler beam) 
+\tNodes: {}\n\t       {}\n\t",
+            self.id, self.sec_area, self.nodes[0], self.nodes[1],
+        )
     }
 }
