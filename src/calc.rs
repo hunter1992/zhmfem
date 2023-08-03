7e35aed626401a6d851340deefed0183d21ae320
zhm <1101510340zhm@gmail.com>
2023-08-03 10:03:24 +0800

    Beam1D2N elem done

--- src/calc.rs
+++ src/calc.rs
@@ -4,15 +4,6 @@ use crate::Dtype;
 use na::*;
 use std::time::Instant;
 
-/*
-pub struct NonLinearEqs<const N: usize> {
-    state: bool,
-    pub disps: [Dtype; N],
-    pub forces: [Dtype; N],
-    pub static_kmat: [[Dtype; N]; N],
-}
-*/
-
 /// Linear equations: A*x = b,
 /// In this case: A for static_kmat, x for disps, b for forces
 pub struct LinearEqs<const N: usize> {
