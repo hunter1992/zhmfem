1b209f69e48ff69dcb07f2b101eacb11f3a21ce7
zhm <1101510340zhm@gmail.com>
2023-08-21 16:32:57 +0800

    Rod2D2N 10 bar example done.

--- examples/rod2D_10bar.rs
+++ examples/rod2D_10bar.rs
@@ -1,4 +1,5 @@
-/// 算例来源：曾攀《有限元基础教程》3.2.5(1)中的四杆桁架结构
+/// 算例来源：左文杰《一维有限单元法》7.4节中的十杆桁架结构
+/// 注：此例中的单位为英寸-磅
 use std::collections::HashMap;
 //use std::io::{BufWriter, Write};
 use std::time::Instant;
@@ -8,24 +9,38 @@ use zhmfem::*;
 fn main() {
     let time_start = Instant::now();
 
-    let section_area: [Dtype; 4] = [100.0; 4];
-    let material = (295000.0 as Dtype, 0.25 as Dtype);
+    let section_area: [Dtype; 10] = [10.0; 10];
+    let material = (10000000.0 as Dtype, 0.25 as Dtype);
 
     const R: usize = 2;
-    const C: usize = 2;
+    const C: usize = 3;
     const M: usize = 2;
     const F: usize = 2;
 
+    let l: Dtype = 360.0;
     let points: Vec<Vec<Dtype>> = vec![
+        vec![0.0, l],
+        vec![l, l],
+        vec![2.0 * l, l],
+        vec![2.0 * l, 0.0],
+        vec![l, 0.0],
         vec![0.0, 0.0],
-        vec![400.0, 0.0],
-        vec![400.0, 300.0],
-        vec![0.0, 300.0],
     ];
-    let cpld = vec![vec![0, 1], vec![2, 1], vec![0, 2], vec![3, 2]];
-    let zero_disp: Vec<usize> = vec![0, 1, 3, 6, 7];
-    let force_idx: Vec<usize> = vec![2, 5];
-    let force_vlu: Vec<Dtype> = vec![20000.0, -25000.0];
+    let cpld = vec![
+        vec![0, 1],
+        vec![5, 4],
+        vec![1, 2],
+        vec![4, 3],
+        vec![0, 4],
+        vec![5, 1],
+        vec![1, 3],
+        vec![4, 2],
+        vec![1, 4],
+        vec![2, 3],
+    ];
+    let zero_disp: Vec<usize> = vec![0, 1, 10, 11];
+    let force_idx: Vec<usize> = vec![2, 3, 4, 5, 6, 7, 8, 9];
+    let force_vlu: Vec<Dtype> = vec![0., 0., 0., 0., 0., -100000.0, 0., -100000.0];
     let force_data: HashMap<usize, Dtype> =
         force_idx.into_iter().zip(force_vlu.into_iter()).collect();
 
@@ -35,23 +50,21 @@ fn main() {
 
     let mut part1: Part2D<Rod2D2N, { R * C }, F, M> = Part2D::new(1, &nodes, &mut rod_vec, &cpld);
     part1.k(material);
-    part1.k_printer(3.0);
+    part1.k_printer(4.0);
 
     let mut eqs: LinearEqs<{ R * C * F }> =
         LinearEqs::new(part1.disps(), part1.forces(), *part1.k(material));
 
     eqs.lu_direct_solver();
+    //eqs.gauss_seidel_iter_solver(0.00001); // 耗时:1.889097ms,迭代:143次,误差:0.000009
 
     part1.write_result(&eqs);
 
     print_1darr("qe", &part1.disps(), 0.0);
-    print_1darr("fe", &part1.forces(), 0.0);
+    print_1darr("fe", &part1.forces(), 4.0);
 
     for rod in rod_vec.iter() {
-        print!(
-            "rod's stress:\n{:?}\n",
-            rod.stress([1.0, 2.0, 3.0], material),
-        );
+        print_1dvec("stress", &rod.stress([1., 1., 1.], material), 4.);
     }
 
     /*
