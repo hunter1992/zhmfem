e98b00f5b9093b226bcdf56815cd4db02448eb69
zhm <1101510340zhm@gmail.com>
2023-12-03 23:34:37 +0800

    fix welcome

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("");
     println!("      ========================================");
-    println!("      ========== WELCOME tO zhmfem! ==========");
+    println!("      ========== WELCOME to zhmfem! ==========");
     println!("      ========================================\n");
     println!(">>> check the examples under 'zhmfem/examples/' path using:");
     println!("        cargo run --examples <example-name>");
